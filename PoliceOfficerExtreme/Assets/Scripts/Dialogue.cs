fc2949f3a06d97ebd335b7c43d21a60fc9df9ba4
Simon Blasen <Simon.yzf@t-online.de>
2021-11-17 17:47:44 +0100

    Some stuff

--- PoliceOfficerExtreme/Assets/Scripts/Dialogue.cs
+++ PoliceOfficerExtreme/Assets/Scripts/Dialogue.cs
@@ -1,28 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI textMesh = null;
+    [SerializeField]
+    private float secondsPerLetter = 0.1f;
+
+
+    private float counter = 0f;
+
+    private string textRem = "";
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        textMesh.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (counter > 0f)
+        {
+            counter -= Time.deltaTime;
+        }
+        else
+        {
+            if (textRem.Length > 0)
+            {
+                textMesh.text += textRem.Substring(0, 1);
+                textRem = textRem.Substring(1);
+
+                counter = secondsPerLetter;
+            }
+        }
     }
 
     public void ShowDialogueText(string text)
     {
-
+        textRem = text;
+        textMesh.text = "";
+        counter = 0f;
     }
 
     public void StopShowingText()
     {
-
+        textRem = "";
+        textMesh.text = "";
     }
 }
