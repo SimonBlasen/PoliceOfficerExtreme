54b242cf29f8912475c28185f094207a46de9c22
Simon Blasen <Simon.yzf@t-online.de>
2021-11-19 17:01:00 +0100

    Fixed motor sound script

--- PoliceOfficerExtreme/Assets/MotorSoundManager.cs
+++ PoliceOfficerExtreme/Assets/MotorSoundManager.cs
@@ -22,19 +22,26 @@ public class MotorSoundManager : MonoBehaviour
     {
        
 
-        pitch = minPitch + (pitchMult * (Mathf.Abs(carRig.velocity.x) + Mathf.Abs(carRig.velocity.z)));
+        // Das .magnitude kann man auf jedem Vector3 aufrufen, und gibt einfach die Länge des Vektors aus. In dem Fall ist das smoother, als Abs(x)+Abs(z) zu nehmen, was aber auch in etwa funktionieren würde :D
 
-        if (motorAudio.pitch <= minPitch)
+        //pitch = minPitch + (pitchMult * (Mathf.Abs(carRig.velocity.x) + Mathf.Abs(carRig.velocity.z)));
+        pitch = minPitch + (pitchMult * carRig.velocity.magnitude);
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+
+
+        // Hier hast du in der if motorAudio.pitch geprüft, aber du müsstest pitch prüfen, ob es größer oder kleiner als min-max pitch ist
+
+        /*if (motorAudio.pitch <= minPitch)
         {
             pitch = minPitch;
         }
         if(motorAudio.pitch >= maxPitch)
         {
             pitch = maxPitch;
-        }
+        }*/
 
-        Debug.Log(pitch);
-        Debug.Log(Mathf.Abs(carRig.velocity.x));
+        //Debug.Log(pitch);
+        //Debug.Log(Mathf.Abs(carRig.velocity.x));
         motorAudio.pitch = pitch;
     }
 }
